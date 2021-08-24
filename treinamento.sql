8cc530c1592cf1f1ef9fef9e2ef0f4c0db7d5be5
Rodrigo Albuquerque <albuquerque730@gmail.com>
2021-08-24 09:55:39 -0300

    estudando sql

--- treinamento.sql
+++ treinamento.sql
@@ -13,7 +13,7 @@ create table livros(
 insert into livros values ('banco de dados','roddrigo albuquerque','M',300,'laisayrla',30.30,'MA','2019');
 insert into livros values ('asterisk','roddrigo albuquerque','M',300,'laisayrla',30.30,'MA','2019');
 insert into livros values ('macacos','roddrigo albuquerque','M',600,'laisayrla',40.30,'MA','2018');
-insert into livros values ('dev','tiago','M',600,'laisayrla',40.30,'MA','2017');
+insert into livros values ('dev','tiago','M',600,'devmedia',40.30,'MA','2017');
 
 select LIVRO from livros where SEXO='M' and AUTOR = 'roddrigo albuquerque';
 select count(*) as "registros da minha tabela" from livros;
